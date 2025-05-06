1e639cdc80ededb43fddd66bd5b7d4a491ddaf31
ScottIngram <github@ScottIngram.org>
2025-05-06 10:11:12 -0700

    support passing of args into the throttled function

--- Throttler.lua
+++ Throttler.lua
@@ -8,7 +8,7 @@
 
 local ADDON_NAME, ADDON_SYMBOL_TABLE = ...
 ADDON_SYMBOL_TABLE.Wormhole()
-local zebug = Zebug:new(Zebug.OUTPUT.WARN)
+local zebug = Zebug:new(Zebug.OUTPUT.INFO)
 
 ---@class Throttler
 ---@field t0 number time of previous execution
@@ -45,10 +45,16 @@ function Throttler:new(func, maxFreq, id)
     return zelf
 end
 
+function Throttler:asFunc()
+    return function(...)
+        self:exe(...)
+    end
+end
+
 -- execute eventually (perhaps immediately) depending on the maxFreq param given to new()
 ---@param reportedElapsed number (optional) how long since this was last called (useful when the Bliz handlers provide ie, eg OnUpdate always has an "elapsed" param
 ---@return boolean true if the func was executed immediately.  false if the func was postponed or simply thrown away because it's already in the q
-function Throttler:exe(reportedElapsed)
+function Throttler:exe(...)
     local elapsed
     local runNow
     local previousTime = self.t0
@@ -65,7 +71,7 @@ function Throttler:exe(reportedElapsed)
 
     if runNow then
         zebug.info:ifMe1st(self.id):out(5,div, "t", time(), "Immediately EXE", self.id, "elapsed",elapsed)
-        self:doItNow()
+        self:doItNow(...)
         return true
     else
         -- do it later
@@ -78,10 +84,14 @@ function Throttler:exe(reportedElapsed)
             local scheduledAt = time()
             exeId = exeId + 1
             zebug.info:ifMe1st(self.id):out(5,div, "t", time(), "Scheduling for later", self.id, "elapsed",elapsed, "runWhen",runWhen, "xId", exeId)
-            C_Timer.After(runWhen, function()
+
+            -- hopefully this pack/unpack won't impose TOO much of a performance hit.
+            -- It happens at most only once within a maxFreq period
+            local capturedArgs = {...}
+            C_Timer.After(runWhen, function() -- using ... here is fail.  No args are passed from After into the func
                 local prolapsed = time() - scheduledAt
                 zebug.info:ifMe1st(self.id):name("exe"):out(7,div, "t", time(), "delayed EXE", self.id, "prolapsed", prolapsed, "xId", exeId)
-                self:doItNow()
+                self:doItNow(unpack(capturedArgs), "delayed")
             end)
         end
     end
@@ -89,8 +99,8 @@ function Throttler:exe(reportedElapsed)
     return false
 end
 
-function Throttler:doItNow()
-    self.func()
+function Throttler:doItNow(...)
+    self.func(...)
     self.t0 = time() -- reset T0 to NOW!
     self.isQueued = false
 end
